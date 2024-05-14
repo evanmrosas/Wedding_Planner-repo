b9393f8653cd7a3803111c5d62cecd5d9b5e197b
Ashleigh Pippin <pippin.ashleigh@gmail.com>
2024-05-14 16:15:39 -0400

    gift registry 1st commit

---
+++ flask_app/controllers/gifts.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, redirect, request, session, flash
+from server import app
+from flask_app.models import gift
+
+#show all
+@app.route("/giftregistry")
+def giftregistry():
+    if "userID" not in session:
+        flash("Please log in.", "login")
+        return redirect("/")
+    gifts = gift.Gift.getAllGifts()
+    print(gifts)
+    return render_template("giftregistry.html", gifts = gifts)
+
+#create new gift form
+@app.route("/giftregistry/new")
+def new_gift_form():
+    if "userID" not in session:
+        flash("Please log in.", "login")
+        return redirect("/")
+    return render_template("newgift.html")
+
+#create new gift ACTION
+@app.route("/giftregistry/create", methods=["POST"])
+def create_gift():
+    if "userID" not in session:
+        flash("Please log in.", "login")
+        return redirect("/")
+    if not gift.Gift.validate_gift(request.form):
+        return redirect("/giftregistry/create")
+    data = {
+        "name": request.form["name"],
+    }
+    gift.Gift.save(data)
+    return redirect("/giftregistry")
+
+#update gift Form
+@app.route('/giftregistry/edit/<int:id>')
+def edit_gift(id):
+    if "userID" not in session:
+        flash("Please log in.", "login")
+        return redirect("/")
+    data={ 
+        "id" : id
+    }
+    gift_id = gift.Gift.get_one_gift(data)
+    return render_template("editgift.html",gift=gift_id)
+
+#update gift ACTION
+@app.route('/giftregistry/update/<int:id>',methods=['POST'])
+def update_gift():
+    if "userID" not in session:
+        flash("Please log in.", "login")
+        return redirect("/")
+    if not gift.Gift.validate_gift(request.form):
+        return redirect(f"/recipe/edit/{request.form['id']}")
+    data = {
+        "name" : request.form["name"],
+        "id" : id
+    }
+    gift.Gift.update_gift(data)
+    return redirect("/giftregistry")
+
+#delete gift 
+@app.route("/giftregistry/delete/<int:id>")
+def delete_gift(id):
+    if "userID" not in session:
+        flash("Please log in.", "login")
+        return redirect("/")
+    data={ 
+        "id" : id
+    }
+    gift.Gift.delete(id)
+    return redirect("/giftregistry")
+
---
+++ flask_app/models/gift.py
@@ -0,0 +1,103 @@
+#import mysql connection
+from flask_app.config.mysqlconnection import connectToMySQL
+
+#import flash for error messages
+from flask import Flask, flash
+#import user model for including in methods 
+from flask_app.models import user
+#import re for validation just in case needed
+import re
+
+
+class Gift:
+    db= "wedding_schema"
+    def __init__(self, data):
+        self.id = data["id"]
+        self.name = data["name"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["created_at"]
+
+
+
+#create
+    @classmethod
+    def save(cls, data):
+        query = """
+        INSERT INTO recipes (name) 
+        VALUES (%(name)s);
+        """
+        results = connectToMySQL(cls.db).query_db(query, data)
+        if results:
+            return results
+
+
+
+#read one
+    @classmethod
+    def get_one_gift(cls, id):
+        data = {
+            "id" : id
+        }
+        query="SELECT * FROM recipes LEFT JOIN users ON user_id = users.id WHERE gifts.id = %(id)s;"
+        results = connectToMySQL(cls.db).query_db(query, data)
+        gift = cls(results[0])
+
+        user_data ={
+                "id" : results[0]["users.id"],
+                "first_name" : results[0]["first_name"],
+                "last_name" : results[0]["last_name"],
+                "email" : results[0]["email"],
+                "password" : results[0]["password"],
+                "created_at" : results[0]["users.created_at"],
+                "updated_at" : results[0]["users.updated_at"] 
+        }
+        gift.user = user.User(user_data)
+        return gift
+
+
+
+#read all
+#read
+    @classmethod 
+    def getAllGifts(cls):
+        query = "SELECT * FROM gifts;"
+        results = connectToMySQL(cls.db).query_db(query)
+        gifts = []
+        for gift in results:
+            gifts.append(cls(gift))
+        return gifts
+
+
+#update
+    @classmethod
+    def update_gift(cls, data):
+        query="UPDATE recipes SET name = %(name)s WHERE id = %(id)s;"
+        results = connectToMySQL(cls.db).query_db(query, data)
+        if results:
+            return results
+
+
+
+#delete 
+    @classmethod
+    def delete_gift(cls, id):
+        data = {
+            "id" : id
+        }
+        query= "DELETE FROM gifts WHERE id = %(id)s;"
+        results = connectToMySQL(cls.db).query_db(query, data)
+        if results:
+            return results
+
+
+
+#validate
+    @staticmethod
+    def validate_gift(gift):
+        is_valid = True
+        if len(gift['name']) <1:
+            flash("Gift must have a name", "gift")
+            is_valid = False
+
+
+        return is_valid
