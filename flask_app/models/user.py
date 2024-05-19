4f6930421ca97cdc1fa2404d6c284b23d842237b
devincohen <devin94cohen@gmail.com>
2024-05-19 15:03:44 -0500

    finished all validations, edit, etc

---
+++ flask_app/models/user.py
@@ -0,0 +1,90 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+pattern = r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$"
+
+
+class User:
+    db= 'planner_db'
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+    @classmethod
+    def save_user(cls,data):
+        print('====in save user method user model ====')
+        query = """INSERT INTO users (first_name, last_name, email, password)
+                VALUES(%(first_name)s,%(last_name)s,%(email)s,%(password)s)"""
+        result = connectToMySQL(cls.db).query_db(query,data)
+        return result
+    
+    @classmethod
+    def update_user(cls,data):
+        ('====in update user method user model ====')
+        query="""UPDATE users SET first_name=%(first_name)s,
+                last_name=%(last_name)s,email=%(email)s
+                WHERE id = %(id)s;"""
+        result = connectToMySQL(cls.db).query_db(query,data)
+        return result
+    
+    @classmethod
+    def get_one_by_id(cls,data):
+        print('====get one by id user model ====')
+        query="""SELECT * FROM users WHERE id = %(id)s """
+        result = connectToMySQL(cls.db).query_db(query,data)
+        print(result)
+        if result:
+            return cls(result[0])
+        
+    @classmethod
+    def get_one_by_email(cls,data):
+        print('====get by email user model====')
+        query = """SELECT * FROM users WHERE email = %(email)s;"""
+        results = connectToMySQL(cls.db).query_db(query,data)
+        if len(results) <1 :
+            return False
+        return cls(results[0])
+    
+    @classmethod
+    def delete_user(cls,data):
+        print('==== in delete method user model ====')
+        query="DELETE FROM users WHERE id = %(id)s"
+        result =  connectToMySQL(cls.db).query_db(query,data)
+        return result
+    
+
+
+    @staticmethod
+    def is_valid_register(user_info):
+        is_valid = True
+        if len(user_info["first_name"]) <=1:
+            flash("First name required! Must have 2 characters","reg")
+            is_valid=False
+        if len(user_info["last_name"]) <=1:
+            flash("Last name required! Must have 2 characters","reg")
+            is_valid = False
+        if len(user_info["email"]) <=1:
+            flash("Email name required!","reg")
+        if not EMAIL_REGEX.match(user_info['email']): 
+            flash("Invalid email address!","reg")
+            is_valid = False
+        if len(user_info["password"]) <=7:
+            flash("Password required / Or to weak!","reg")
+            is_valid = False
+        if user_info["confirm_password"] != user_info["password"]:
+            flash("Password must match!","reg")
+            is_valid = False
+        if not re.match(pattern, user_info['password']):
+            flash("Password must contain an uppercase and lowercase letter and a number.","reg")
+            is_valid = False
+        print("validating user is valid:", is_valid)
+        
+        return is_valid
--- flask_app/models/wedding.py
+++ flask_app/models/wedding.py
@@ -1,7 +1,9 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 
+from flask import flash
+
 class Wedding:
-    db="wedding_schema"
+    db="planner_db"
     def __init__(self, data):
         self.id = data["id"]
         self.partner_name_1 = data["partner_name_1"]
@@ -28,7 +30,7 @@ class Wedding:
 
     @classmethod
     def save(cls, data):
-        query = """INSERT INTO  --database--(partner_name_1, partner_name_2, location, date, reception, total_guest, notes, user_id, created_at, updated_at) 
+        query = """INSERT INTO  weddings (partner_name_1, partner_name_2, location, date, reception, total_guest, notes, user_id, created_at, updated_at) 
                 VALUES ( %(partner_name_1)s, %(partner_name_2)s, %(location)s, %(date)s, %(reception)s, %(total_guest)s, %(notes)s, %(user_id)s, NOW(), NOW())
             """
         return connectToMySQL(cls.db).query_db(query, data)
@@ -37,9 +39,11 @@ class Wedding:
     def get_all_for_user(cls, user_id):
         query = """SELECT * from weddings 
                 WHERE user_id = %(user_id)s """
-        data = {"id" : user_id}
+        data = {"user_id" : user_id}
         result = connectToMySQL(cls.db).query_db(query, data)
-        return cls(result)
+        if not result:
+            return None
+        return cls(result[0])
         
 
     @classmethod
@@ -54,7 +58,7 @@ class Wedding:
     @classmethod
     def update(cls, data):
         query = """UPDATE weddings
-                SET partner_name_1 = %(partner_name_1)s, partner_name_2%(partner_name_2)s, location = %(location)s, date = %(date)s, recption = %(reception)s, total_guest = %(total_guest)s, notes = %(notes)s, user_id = %(user_id)s
+                SET partner_name_1 = %(partner_name_1)s, partner_name_2=%(partner_name_2)s, location = %(location)s, date = %(date)s, reception = %(reception)s, total_guest = %(total_guest)s, notes = %(notes)s, user_id = %(user_id)s
             """
         return connectToMySQL(cls.db).query_db(query, data)
     
@@ -64,3 +68,28 @@ class Wedding:
                 WHERE id = %(wedding_id)s """
         data = {"id" : wedding_id}
         return connectToMySQL(cls.db).query_db(query, data)
+    
+
+    @staticmethod
+    def validate_wedding(wedding):
+        is_valid = True
+        if len(wedding['partner_name_1']) <=2:
+            flash("Partner 1's name must be at least 2 characters long",  'partner_name_1')
+            is_valid = False
+        if len(wedding['partner_name_2']) <=2:
+            flash("Partner 2's name must be at least 2 characters long", 'partner_name_2')
+            is_valid = False
+        if len(wedding['location']) <=2:
+            flash("Location must be at least 2 characters long", 'location')
+            is_valid = False
+        if not wedding['date']:
+            flash("Date must be valid", 'date')
+            is_valid = False
+        if len(wedding['reception']) <=2:
+            flash("Location must be at least 2 characters long", 'reception')
+            is_valid = False
+        if wedding['total_guest'] == "" or int(wedding['total_guest']) is None or int(wedding['total_guest']) < 0:
+            flash("Number of guests must be valid", 'total_guest')
+            is_valid = False
+
+        return is_valid
