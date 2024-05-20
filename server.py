4e4707dd90261ba04a663a63a251a898de430d03
Ashleigh Pippin <pippin.ashleigh@gmail.com>
2024-05-19 20:10:24 -0400

    server changes

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask_app.controllers import budgets
+from flask_app.controllers import budgets, gifts, guests, users, weddings
 
 if __name__ == "__main__":
     app.run(debug=True)
