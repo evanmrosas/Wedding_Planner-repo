8c2b90e106c2fce1cec5d6b1bb43c6ce87e1bb14
Ashlie Ernst <ashlieernst22@gmail.com>
2024-05-19 19:01:33 -0500

    edited user_id

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -21,11 +21,6 @@ def login_form():
     return render_template('/login.html')
 
 
-@app.route('/dashboard')
-def dashboard():
-
-    return render_template('/dashboard.html')
-
 
 @app.route('/register', methods=['POST'])
 def register():
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask_app.controllers import budgets
+from flask_app.controllers import budgets, users, gifts, guests, weddings
 
 if __name__ == "__main__":
     app.run(debug=True)
